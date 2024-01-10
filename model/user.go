7913705dccf337576cf504e7fbfbf43d30947123
Việt Stars <vietstar.nt@gmail.com>
2024-01-10 16:23:58 +0700

    Add files via upload

--- model/user.go
+++ model/user.go
@@ -4,20 +4,30 @@ import (
   "go-gin-restful/database"
   "html"
   "strings"
+  "time"
+  // "fmt"
 
   "golang.org/x/crypto/bcrypt"
+  "gorm.io/plugin/dbresolver"
+  "gorm.io/sharding"
   "gorm.io/gorm"
 )
 
 type User struct {
   gorm.Model
+  ID int64 `gorm:"primarykey"`
   Username string `gorm:"size:255;not null;unique" json:"username"`
   Password string `gorm:"size:255;not null;" json:"-"`
-  Entries  []Entry `gorm:"foreignKey:UserID"`
+  Email string `gorm:"size:255;not null;unique" json:"email"`
+  Avatar string `gorm:"size:255;" json:"avatar"`
+  Entries  []Entry `gorm:"foreignKey:UserID; constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+  CreatedAt time.Time `gorm:"autoCreateTime:true" json:"CreatedAt"`
+  UpdatedAt time.Time `gorm:"autoUpdateTime:true" json:"UpdatedAt"`
+  DeletedAt *time.Time `json:"-"`
 }
 
 func (user *User) Save() (*User, error) {
-  err := database.Database.Create(&user).Error
+  err := database.DB.Create(&user).Error
   if err != nil {
     return &User{}, err
   }
@@ -25,7 +35,6 @@ func (user *User) Save() (*User, error) {
 }
 
 func (user *User) BeforeSave(*gorm.DB) error {
-
   passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
   if err != nil {
     return err
@@ -35,25 +44,56 @@ func (user *User) BeforeSave(*gorm.DB) error {
   return nil
 }
 
-
 func (user *User) ValidatePassword(password string) error {
   return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 }
 
 func FindUserByUsername(username string) (User, error) {
   var user User
-  err := database.Database.Where("username=?", username).Find(&user).Error
+  err := database.DB.Where("username", username).Find(&user).Error
   if err != nil {
     return User{}, err
   }
   return user, nil
 }
 
-func FindUserById(id uint) (User, error) {
+func FindUserByEmail(email string) (User, error) {
   var user User
-  err := database.Database.Preload("Entries").Where("ID=?", id).Find(&user).Error
+  err := database.DB.Where("email", email).Find(&user).Error
   if err != nil {
     return User{}, err
   }
   return user, nil
 }
+
+func FindUserById(id int64) (User, error) {
+  var user User
+  err := database.DB.Preload("Entries", func(dbs *gorm.DB) *gorm.DB {
+
+    dbs.Use(dbresolver.Register(dbresolver.Config{
+      Replicas: []gorm.Dialector{database.DBR.Dialector},
+    }))
+
+    dbs.Use(sharding.Register(sharding.Config{
+      ShardingKey: "user_id",
+      NumberOfShards: 26,
+      PrimaryKeyGenerator: sharding.PKSnowflake,
+    }, "entries"))
+
+    return dbs.Raw("SELECT * FROM entries WHERE user_id = ? order by updated_at DESC", id)
+  }).Where("id", id).Find(&user).Error
+
+  if err != nil {
+    return User{}, err
+  }
+  return user, nil
+}
+
+func (user *User) EditUser() (*User, error) {
+  err := database.DB.Save(user).Error
+  if err != nil {
+    return &User{}, err
+  }
+  return user, nil
+}
+
