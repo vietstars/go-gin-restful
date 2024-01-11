a3c931590b5113dca0e3609d1ec2ffa7efc61fc0
Việt Stars <vietstar.nt@gmail.com>
2024-01-11 10:55:59 +0700

    Update user.go

--- controller/user.go
+++ controller/user.go
@@ -6,8 +6,13 @@ import (
   "net/http"
 
   "github.com/gin-gonic/gin"
-  // "time"
-  // "fmt"
+  "path/filepath"
+  "strings"
+  "time"
+  "fmt"
+  "log"
+  "io"
+  "os"
 )
 
 func EditProfile(context *gin.Context) {
@@ -37,4 +42,56 @@ func EditProfile(context *gin.Context) {
   }
 
   context.JSON(http.StatusCreated, gin.H{"data": updatedUser})
+}
+
+func EditAvatar(context *gin.Context) {
+  user, err := helper.CurrentUser(context)
+
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    fmt.Println("error -2")
+    return
+  }
+
+  file, header, err := context.Request.FormFile("image")
+  if err != nil {
+    context.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+    fmt.Println("error -1")
+    return
+  }
+
+  fileExt := filepath.Ext(header.Filename)
+  originalFileName := strings.TrimSuffix(filepath.Base(header.Filename), filepath.Ext(header.Filename))
+  now := time.Now()
+  filename := strings.ReplaceAll(strings.ToLower(originalFileName), " ", "-") + "-" + fmt.Sprintf("%v", now.Unix()) + fileExt
+  filePath := fmt.Sprintf("%s/%s/%s", os.Getenv("BASE_URL"), os.Getenv("IMG_DIR"), filename)
+
+  out, err := os.Create("./web-app/public/images/" + filename)
+  if err != nil {
+    log.Fatal(err)
+    context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    fmt.Println("error 1")
+    return
+  }
+  defer out.Close()
+
+  _, err = io.Copy(out, file)
+  if err != nil {
+    log.Fatal(err)
+    context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    fmt.Println("error 2")
+    return
+  }
+
+  user.Avatar = filePath
+
+  updatedUser, err := user.EditUser()
+
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    fmt.Println("error 3")
+    return
+  }
+
+  context.JSON(http.StatusOK, gin.H{"data": updatedUser})
 }
